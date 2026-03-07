6fbe9bc3182e4c6d5b0ae79fc314ef02cc71604b
BONY MATHEW THOMAS <78587059+bonym007@users.noreply.github.com>
2026-03-07 07:25:53 +0000

    operations

--- 01-Basics/03-conversionOperation.js
+++ 01-Basics/03-conversionOperation.js
@@ -18,6 +18,7 @@ Number(null) --> 0
 Number(undefined) --> NaN
 Number(true) --> 1
 Number(false) --> 0
+Number("") --> 0
 -----------------------------------------------
 Boolean(1) --> true
 Boolean(0) --> false
@@ -32,4 +33,32 @@ String(null) --> null
 
 */
 
-console.log(typeof String(null));
+// console.log(typeof String(null));
+
+//***************************** Operations **************************/
+
+/*
+let value = 3;
+let negValue = -value;
+console.log(negValue);      --> -3
+
+let str1 = "hello";
+let str2 = " Hitesh";
+let str3 = str1 + str2;
+console.log(str3);          --> hello Hitesh
+
+
+console.log(1 + "hello");       --> 1hello  --> number concatenated witht string
+console.log("1" + 2 + 2 + 2);   --> 1222    --> since first value is string, consider everyting as string
+console.log(1 + 2 + "2");       --> 32      --> perform operation on numbers and cancatenate the strinf
+console.log(1 + "2" + 2)        --> 122     --> number concatednated with (things comming after string is als considerd as string)
+
+
+
+console.log(true)   --> true
+console.log(+true)  --> 1       --> short hand to convet to Number
+console.log(+"")    --> 0       --> shory hand to conert to Number
+
+*/
+
+console.log(Number(true));
